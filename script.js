f7c34ffe461a607ff47d3e43391005bc5fba970e
Webhusn <96583400+Webhusn@users.noreply.github.com>
2022-01-26 21:12:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var number = 50;
+if (number > 10) {
+    console.log("The number is greater than 10");
+} else if (number === 10) {
+    console.log("The number is equal to 10");
+} else {
+    console.log("num is niether greater than nor equal to 10");
+}
---
+++ script2.js
@@ -0,0 +1,14 @@
+var a = 20;
+var b = 40;
+var c = 60;
+var grater;
+if (a > b) {
+    g = a;
+}
+else {
+    grater = b;
+}
+if (c > grater) {
+    g = c;
+}
+console.log(grater);
