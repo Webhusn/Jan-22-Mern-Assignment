885e46e3e552f581f08643f780196fae8d2f1dee
Webhusn <96583400+Webhusn@users.noreply.github.com>
2022-02-06 16:30:55 +0530

    Update 25-jan-assignment.js

--- 25-jan-assignment.js
+++ 25-jan-assignment.js
@@ -2,7 +2,7 @@ var number = 50;
 if (number > 10) {
     console.log("The number is greater than 10");
 } else if (number === 10) {
-    console.log("The number is equal to 10");
+    prompt("The number is equal to 10");
 } else {
-    console.log("num is niether greater than nor equal to 10");
+    prompt("num is niether greater than nor equal to 10");
 }
