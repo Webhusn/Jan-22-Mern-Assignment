d2a7bdae8b69ab9360009452487b5bc066f07e25
Webhusn <96583400+Webhusn@users.noreply.github.com>
2022-02-06 16:41:08 +0530

    Update 25-jan-assignment.js

--- 25-jan-assignment.js
+++ 25-jan-assignment.js
@@ -1,10 +1,10 @@
 <script>
 var number = 50;
 if (number > 10) {
-    prompt("The number is greater than 10");
+   alert("The number is greater than 10");
 } else if (number === 10) {
-    prompt("The number is equal to 10");
+    alert("The number is equal to 10");
 } else {
-    prompt("num is niether greater than nor equal to 10");
+    alert("num is niether greater than nor equal to 10");
 }
 </script>
