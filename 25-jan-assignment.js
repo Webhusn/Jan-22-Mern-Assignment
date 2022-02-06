efc510916dab44a42794ef91030129f4244c5796
Webhusn <96583400+Webhusn@users.noreply.github.com>
2022-02-06 16:32:24 +0530

    Update 25-jan-assignment.js

--- 25-jan-assignment.js
+++ 25-jan-assignment.js
@@ -1,3 +1,4 @@
+<script>
 var number = 50;
 if (number > 10) {
     console.log("The number is greater than 10");
@@ -6,3 +7,4 @@ if (number > 10) {
 } else {
     prompt("num is niether greater than nor equal to 10");
 }
+</script>
